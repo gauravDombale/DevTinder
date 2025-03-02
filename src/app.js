a85d697dd98fd49408e29fe2e671f95cc5abfad3
Gaurav Dombale <gauravdombale@Gauravs-MacBook-Air.local>
2025-03-02 14:43:20 +0530

    explored route handlers and http methods

--- src/app.js
+++ src/app.js
@@ -1,19 +1,28 @@
 const express = require("express");
 const app = express();
 
-//! This function is known as request handler, here /test is the api endpoint or route
-app.use("/test",(req, res) => {
-  res.send("test");
-});
+//get user
+app.get("/user", (req, res)=>{
+    res.send("get user");
+})
 
-app.use("/test2",(req, res) => {
-  res.send("test2");
-});
+// post user
+app.post("/user", (req, res)=>{
+    res.send("post user");
+})
 
-app.use((req, res) => {
-    res.send("hello from server 3000");
-  });
-  
+// delete user
+app.delete("/user", (req, res)=>{
+    res.send("delete user");
+})
+
+// put user
+app.put("/user", (req, res)=>{
+    res.send("put user");
+})
+
+
+
 
 app.listen(3000, () => {
   console.log("server is running on port 3000");
