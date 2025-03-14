1d2b9bb8e66b757bee5f0f921f4e9d205589d0fd
gauravDombale <gauravdombale007@gmail.com>
2025-03-14 11:21:09 +0530

    code added

--- src/app.js
+++ src/app.js
@@ -1,13 +1,13 @@
 const express = require("express");
-require('dotenv').config();
+require("dotenv").config();
 const connectDB = require("./config/database.js");
 const User = require("./models/user.js");
 const app = express();
 
-//! Middleware to read JSON data from the request body
+//!Middleware to read JSON data from the request body
 app.use(express.json());
 
-
+//* signup
 app.post("/signup", async (req, res) => {
   const user = new User(req.body);
 
@@ -20,7 +20,7 @@ app.post("/signup", async (req, res) => {
   }
 });
 
-//feed api to fetch all users
+//* feed api to fetch all users
 app.get("/feed", async (req, res) => {
   try {
     const users = await User.find();
@@ -31,8 +31,22 @@ app.get("/feed", async (req, res) => {
   }
 });
 
+//* find by id
+app.get("/users/:id", async (req, res) => {
+  try {
+    const user = await User.findById(req.params.id);
+    if (!user) {
+      return res.status(404).json({ message: "User not found" });
+    }
+    res.json(user);
+    // res.send(user);
+  } catch (error) {
+    console.error("Error fetching user:", error);
+    res.status(500).json({ message: "Error fetching user" });
+  }
+});
 
-//find by mail
+//* find by mail
 app.get("/userEmail", async (req, res) => {
   try {
     const user = await User.findOne({ emailId: req.body.emailId });
@@ -46,6 +60,41 @@ app.get("/userEmail", async (req, res) => {
   }
 });
 
+//* find by id and delete
+app.delete("/users/:id", async (req, res) => {
+  try {
+    const user = await User.findByIdAndDelete(req.params.id);
+    if (!user) {
+      return res.status(404).send("User not found");
+    }
+    res.send("User deleted successfully");
+  } catch (error) {
+    console.error("Error deleting user:", error);
+    res.status(500).send("Error deleting user");
+  }
+});
+
+//* find by id and update
+app.put("/users/:id", async (req, res) => {
+  try {
+    const user = await User.findByIdAndUpdate(
+      req.params.id,
+      req.body,
+      // {new: true, },
+      { returnDocument: "before" }
+    );
+    console.log(user);
+    if (!user) {
+      return res.status(404).send("User not found");
+    }
+    res.send("User updated successfully");
+  } catch (error) {
+    console.error("Error updating user:", error);
+    res.status(500).send("Error updating user");
+  }
+});
+
+//* start server
 const startServer = async () => {
   try {
     await connectDB();
